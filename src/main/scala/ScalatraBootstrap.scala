50c7968689f74fd592b8acd62059e2f90cd6812e
Ed Pond <edward.pond@springer.com>
2015-01-28 13:04:29 +0000

    Demonstrates abstract and fulltext

--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -1,11 +1,25 @@
 import javax.servlet.ServletContext
-import org.scalatra.LifeCycle
-
-import sampleApi.controllers.GreetingController
+import org.scalatra.{ScalatraServlet, LifeCycle}
 
 class ScalatraBootstrap extends LifeCycle {
   override def init(context: ServletContext) {
-    // Mount servlets.
     context.mount(new GreetingController, "/*")
+
+    context.mount(new AbstractController, "/article/*")
+  }
+}
+
+class GreetingController extends ScalatraServlet {
+  get("/") {
+    "Hello world"
   }
 } 
+
+class AbstractController extends ScalatraServlet {
+  get("/(.*)".r) {
+    "Abstract"
+  }
+  get("""\/(.*)\/fulltext.html""".r) {
+    "Fulltext"
+  }
+}
